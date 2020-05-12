09c53da35672a5119ff405f756068a7bdffe80e3
Ivan Tomilov <tiabc.work@gmail.com>
2020-05-12 13:08:31 +0300

    refactor 8.py

--- 8.py
+++ 8.py
@@ -1,15 +1,10 @@
-import binascii
-import math
 import base64
-from Crypto.Cipher import AES
+from block_crypto import *
 
 key_len = 16
 f = open('8.txt', 'r')
 for line in f:
     ct = base64.b64decode(line.strip())
-    for block_start in range(0, len(ct) - key_len, key_len):
-        block = ct[block_start:block_start + key_len]
-        if block in ct[block_start + key_len:]:
-            print('potential ECB: ', binascii.hexlify(ct), ' with repeating block ', binascii.hexlify(block))
-            break
+    if is_potential_ecb(ct, key_len):
+        print('potential ECB: ', binascii.hexlify(ct))
 f.close()
--- block_crypto.py
+++ block_crypto.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import AES
+import binascii
 
 
 def decode_aes_ecb(ct, key):
@@ -9,3 +10,11 @@ def decode_aes_ecb(ct, key):
     for b in ct_blocks:
         res_pt += cipher.decrypt(b)
     return res_pt
+
+
+def is_potential_ecb(ct, key_len):
+    for block_start in range(0, len(ct) - key_len, key_len):
+        block = ct[block_start:block_start + key_len]
+        if block in ct[block_start + key_len:]:
+            return True
+    return False
