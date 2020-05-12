c29c021b448b3e217a8a2976b89a1b1db305a4e0
Ivan Tomilov <tiabc.work@gmail.com>
2020-05-12 13:34:49 +0300

    implement aes_cbc_encrypt

--- block_crypto.py
+++ block_crypto.py
@@ -2,6 +2,22 @@ from Crypto.Cipher import AES
 from common import *
 
 
+def aes_cbc_encrypt(iv, pt, key):
+    key_len = len(key)
+    pt_blocks = [pt[i:i+key_len] for i in range(0, len(pt), key_len)]
+    pt_blocks[-1] = add_pkcs7_padding(pt_blocks[-1], key_len)
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    result = bytearray()
+    prev_pt = iv
+    for pt_block in pt_blocks:
+        xored = xor_repeating_key(pt_block, prev_pt)
+        encrypted = cipher.encrypt(xored)
+        result += encrypted
+        prev_pt = pt_block
+    return result
+
+
 def aes_cbc_decrypt(iv, ct, key):
     key_len = len(key)
     ct_blocks = [ct[i:i+key_len] for i in range(0, len(ct), key_len)]
